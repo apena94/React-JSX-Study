be7ebe02ba63f1f64cbeebc77198363b34b71ba6
Alejandro <alejandropena17@gmail.com>
2021-05-19 13:15:50 -0400

    JSX syntax and structure study.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const getButtonText = () => {
+    return 'Click on me! 22';
+}
+
+// JSX cant show/reference Js object, especialyl where showing text
+
 // Create a react component
 const App = () => {
-    return <div>Hi There!</div>;
+    const buttonText = 'Click Me!'
+
+    return (
+        <div>
+            Hi There!
+            {/* traditional: <button style="background-color: blue, color: white">Submit</button> */}
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+            {/* can use single curly brace, to insert variables */}
+            <button>{buttonText}</button>
+            <button>{getButtonText()}</button>
+        </div>
+    );
 };
 
 // Take the react component and show it on screen(by calling index.html element)
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.querySelector('#root')
 );
 
